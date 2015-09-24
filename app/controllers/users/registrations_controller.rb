437489b0bef89f08bb43ae902ef5e5a312dc2592
phagoth <olexandr.averyanov@gmail.com>
2015-09-24 17:08:16 +0300

    mailer functionality

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -9,15 +9,38 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # POST /resource
   def create
+    build_resource(sign_up_params)
+    has_error = false
+
     if verify_recaptcha
-      super
+      resource.save
+      yield resource if block_given?
+      if resource.persisted?
+        UserMailer.welcome_email(resource).deliver_later
+        if resource.active_for_authentication?
+          set_flash_message :notice, :signed_up if is_flashing_format?
+          sign_up(resource_name, resource)
+          respond_with resource, location: after_sign_up_path_for(resource)
+        else
+          set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_flashing_format?
+          expire_data_after_sign_in!
+          respond_with resource, location: after_inactive_sign_up_path_for(resource)
+        end
+      else
+        has_error = true
+      end
     else
-      build_resource(sign_up_params)
-      clean_up_passwords(resource)
-      flash.now[:alert] = "There was an error with the recaptcha code below. Please re-enter the code."      
-      flash.delete :recaptcha_error
-      render :new
+      has_error = true
     end
+
+    if has_error
+      clean_up_passwords resource
+      flash.now[:alert] = "There was an error with the recaptcha."      
+      flash.delete :recaptcha_error
+      set_minimum_password_length
+      respond_with resource
+    end
+
   end
 
   # GET /resource/edit
--- app/models/user.rb
+++ app/models/user.rb
@@ -15,4 +15,8 @@ class User < ActiveRecord::Base
   def display_name
     "#{first_name} #{last_name[0]}."
   end
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
 end
